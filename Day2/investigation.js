9b13f8c3a01f54a59c27a85c89512235fa464273
Xitr-oss <yayansh2004@gmail.com>
2025-12-09 13:24:13 +0000

    Day2 Classwork investigation of variables

---
+++ Day2/investigation.js
@@ -0,0 +1,59 @@
+// let a=22;
+// const b="abc";
+// var c=78;
+// d=88;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// console.log("2nd question");
+
+// a=25;
+// b="bcd";
+// c=79;
+// d=90;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// console.log("redeclaring the values")
+
+//let a=30;
+// const b="xyz";
+// var c=80;
+// d=95;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+//datatype investigation 
+
+// let a="abc";
+// let b="";
+// let c=null; //**null is an object type 
+// let d;
+// console.log("value of a=>" + a);
+// console.log("value of b=>" + b);
+// console.log("value of c=>" + c);
+// console.log("value of d=>" + d);
+
+// console.log("");
+// console.log("types of values\n")
+
+// console.log("datatype of a=>" + typeof a);
+// console.log("datatype of b=>" + typeof b);
+// console.log("datatype of c=>" + typeof c);
+// console.log("datatype of d=>" + typeof d);
+
+// use strict is used to tell the system that we are using everthing updated
+
+let score="33abcd";
+console.log(typeof score);
+console.log(typeof(score));
+let a = Number(score);  // N is capital (Typecast)
+console.log(a);         //value is NaN
+console.log(typeof(a));
