1a4aa00d582af208c4c91271be9a54565ad38938
Xitr-oss <yayansh2004@gmail.com>
2025-12-11 05:31:14 +0000

    day 4 class completed

---
+++ Day4/day4.js
@@ -0,0 +1,9 @@
+let mydate = new Date();
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleTimeString());
+console.log(mydate.toTimeString());
+console.log(mydate.toUTCString());
+console.log(mydate.toJSON());
